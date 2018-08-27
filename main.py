08a46fc04e0ef44b04e1ce2f09e2641e1dcaefba
Alex DeJong <alexdejong737@gmail.com>
2018-08-27 15:04:51 -0500

    Saved previous contests

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
-import requests
+import requests, pickle, datetime, os.path
 
 url = "https://ctftime.org/event/list/upcoming"
+today = datetime.date.today()
+months = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "June":6,
+          "July":7, "Aug":8, "Sept":9, "Oct":10, "Nov":11, "Dec":12}
+distance = 7
+
+contestFile = 'past_contest_data'
+pastContests = []
+if os.path.exists(contestFile):
+    pastContests = pickle.load(open(contestFile, "rb"))
+maxContestLength = 20
 
 def getWebpageData(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     return requests.get(url, headers=headers).text
 
-def sendEmail():
+def isSoon(date):
+    if (date - today).days < distance:
+        return True
+    else:
+        return False
+
+def sendEmail(name, date):
     pass
 
 data = getWebpageData(url)
 
-table_start = '<table class="table table-striped">'
+table_start = '<table class="table table-striped"><tr><th>Name</th><th>Date</th><th>Format</th><th>Location</th><th>Weight</th><th colspan="2">Notes</th></tr>'
 table_end = '</table>'
 
-data = data[data.index(table_start):]
-data = data[:data.index(table_end)+len(table_end)]
+data = data[data.index(table_start)+len(table_start):]
+data = data[:data.index(table_end)] + "<tr>"
 
-# Loop through each <tr> and parse --> Remove from data
+# Parse each row of HTML <tr>
+
+datalist = []
+while len(data) != 0:
+    index = data.index("<tr>")
+    datalist.append(data[:index])
+    data = data[index+4:]
+    
+datalist = datalist[1:]
+
+for i in range(len(datalist)):
+    # Get important information
+    data = datalist[i]
+    data = data[data.index('">') + 2:]
+    name = data[:data.index("</a")]
+    data = data[data.index('</a></td><td>')+13:]
+    date = data[:data.index('&mdash')]
+
+    # Parse date string
+    day = int(date.split(" ")[0])
+    month = months[date.split(" ")[1][:-2]]
+    date = datetime.date(datetime.date.today().year, month, day)
+
+    # Send email
+    if isSoon(date) and name not in pastContests:
+        sendEmail(name, date)
+        pastContests.append(name)
+        print(name)
+        if len(pastContests) > maxContestLength:
+            pastContests = pastContests[1:]
+
+pickle.dump(pastContests, open(contestFile, "wb+"))
